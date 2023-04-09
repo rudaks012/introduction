f2845ef84d77d875b3bdb317804fa40b7d52e931
gyeongman <rudaks012@naver.com>
2023-04-09 22:23:19 +0900

    Feat : 저장하기 Dto, Entity 생성

--- src/main/java/com/man/introduction/IntroductionApplication.java
+++ src/main/java/com/man/introduction/IntroductionApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableJpaAuditing // JPA Auditing 활성화
 public class IntroductionApplication {
 
     public static void main(String[] args) {
